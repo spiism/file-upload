03ef5798f767ff20776a6f902d39a828fb2d24b2
michael <spiism@gmail.com>
2025-04-09 23:40:34 +1200

    create file type input to upload file

--- src/components/FileUploadTracker.tsx
+++ src/components/FileUploadTracker.tsx
@@ -6,7 +6,10 @@ export default function FileUploadTracker() {
     return (
         <div className="">
             <h1 className="text-xl font-bold mb-4">File Upload Tracker</h1>
-
+            <input
+                type="file"
+                className="mb-4 block w-full"
+            />
         </div>
     );
 }
